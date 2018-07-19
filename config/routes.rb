3b6b0fba65983fefd9710ba273759a4640514b22
djsoul86 <ernestoandrade86@gmail.com>
2018-07-19 18:49:17 -0500

    Se cambian routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   post 'usuario_token' => 'usuario_token#create'
   get 'usuario/current' => 'usuario#current'
   resources :usuarios
-  namespace 'api' do
-    resources :usuarios
-  end
 end

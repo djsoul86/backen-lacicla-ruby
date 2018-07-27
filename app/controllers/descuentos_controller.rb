69b679114f8543344f24ccf6cd04ce26519e23ed
djsoul86 <ernestoandrade86@gmail.com>
2018-07-27 18:39:33 -0500

    se arregla bug

--- app/controllers/descuentos_controller.rb
+++ app/controllers/descuentos_controller.rb
@@ -3,10 +3,8 @@ class DescuentosController < ApplicationController
         if Descuento.where(codigo:params[:codigo]).exists?
             @descuentos = Descuento.where(codigo:params[:codigo])
             render json:{status: 'SUCCESS', message:'Existe',data:@descuentos},status: :ok
-            puts 'existe'
         else
             render json:{status: 'SUCCESS', message:'No existe'},status: :ok
-            puts 'no existe'
         end
     end
 end
